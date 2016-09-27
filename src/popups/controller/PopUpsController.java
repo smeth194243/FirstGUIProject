aec850a5185e38a7e77651d02dba9ee5a0f5b4e8
smeth194243 <smor7432@740s-pl-114.local>
2016-09-27 13:06:50 -0600

    Attempted to get a display window to work.

--- src/popups/controller/PopUpsController.java
+++ src/popups/controller/PopUpsController.java
@@ -1,9 +1,18 @@
 package popups.controller;
 
+import popups.view.PopupViewer;
+
 public class PopUpsController
 {
+	private PopupViewer display;
+	
+	public PopupController()
+	{
+		display = new PopupViewer();
+	}
+	
 	public void start()
 	{
-		System.out.println(" ");
+		display.displayMessage("Look! Words! On the monitor!!!");
 	}
 }
